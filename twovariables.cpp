e9c32dfd458b3e1194b15dfa6b16d90e75c61fbf
92Rehan794 <rehanamanat110@gmail.com>
2024-09-30 16:38:10 +0500

    Add files via upload

---
+++ twovariables.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std; 
+
+main() {
+	int sum = 0, inp;
+	cout << "Enter the first number: ";
+	cin >> inp;
+	sum = sum + inp;
+	cout << "Enter the second number: ";
+	cin >> inp;
+	sum = sum + inp;
+	cout << "Enter the third number: ";
+	cin >> inp;
+	sum = sum + inp;
+	cout << "Enter the fourth number: ";
+	cin >> inp;
+	sum = sum + inp;
+	cout << "Enter the fifth number: ";
+	cin >> inp;
+	sum = sum + inp;
+	cout << sum;
+}
